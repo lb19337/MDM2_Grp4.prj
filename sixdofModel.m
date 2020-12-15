3c8e667930fdc21f60005a272a7c2a666f55c52b
Stephen <ck19955@bristol.ac.uk>
2020-12-15 16:25:46 +0000

    Added Smtrx file, I found it online. I've kept the reference for where I found it. I believe Patriacia used it for her equations.
    I also added the terms in the sixdofModel however they aren't correct so will fix those shortly.

--- sixdofModel.m
+++ sixdofModel.m
@@ -1,6 +1,17 @@
-function [] = sixdofModel()
+function [MRB] = sixdofModel()
 
-% r_g = [10 0 1]';
-% nu = [10 0 1 0 0 1]';
-% I_g = 10000*eye(3);
-% m = 1000
+% Force:
+% X = m(u-v*r+w*q);
+% Y = m(v-w*p+u*r);
+% Z = m(w-u*q+v*p);
+% Moment:
+% K = Ix*p + (Iz-Iy)*qr;
+% M = Iy*q +(Ix-Iz)*rp;
+% N = Iz*r +(Iy-Ix)*pq;
+r_g = [10 0 1]';
+nu = [10 0 1 0 0 1]';
+I_g = 10000*eye(3);
+m = 1000;
+
+MRB = [m*eye(3)     -m*Smtrx(r_g) 
+       m*Smtrx(r_g)    I_g];
