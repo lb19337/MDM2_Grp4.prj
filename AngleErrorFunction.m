601da629404c1c2b8c747281cdf7c0b9629f1fff
Stephen <ck19955@bristol.ac.uk>
2020-12-16 16:53:12 +0000

    Change of axis title, very minor change.

--- AngleErrorFunction.m
+++ AngleErrorFunction.m
@@ -5,6 +5,6 @@ theta = ((initTheta)*exp(-x*(1/(initTheta))))*cos((pi*x)/(initTheta));
 fplot(theta,'color','m')
 xlim([0 (initTheta*4)])
 ylim([(-initTheta*1.5) (initTheta*1.5)])
-ylabel('Theta in Degrees') 
+ylabel('Phi in Degrees') 
 xlabel('Time(s)') 
 grid on
